2075a1d7412008bb858d5a94841f23a96129d118
Amanda Bull <ms.amandabull@outlook.com>
2021-08-29 22:07:19 -0500

    Encapsulation with comments

--- encapsulation.py
+++ encapsulation.py
@@ -1,11 +1,18 @@
+
+#class that uses the protected function
 class Protected: 
+ 
+ 
     def __init__(self):
         self._protectedVar = 0
+
+#object that uses the protected funtion
 
 obj = Protected()
 obj._protectedVar = 34
 print(obj._protectedVar)
 
+#class that uses the private function
 class Protected:
     def __init__(self):
         self.__privateVar = 12
@@ -15,6 +22,8 @@ class Protected:
     
     def setPrivate(self, private):
         self.__privateVar = private
+
+#object that uses the private funtion
 
 obj = Protected()
 obj.getPrivate()
